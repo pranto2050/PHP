a8f8a399cce1d974f594af14a8aeeb84871eef89
pranto-islam <prantoislamnt51@gmail.com>
2025-07-04 15:23:35 +0600

    Add basic HTML structure and content for Operator page

---
+++ Operator/basic.php
@@ -0,0 +1,37 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Operator</title>
+</head>
+<body>
+
+
+<h1 style="color: red; text-align: center;">Operator</h1>
+
+<h3>Basic Type of Operator:</h3>
+<p>1. Unary Operator</p>
+<p>2. Binary Operator</p>
+<p>3. Ternary Operator</p>
+
+<h3>Types of Operator:</h3>
+<p>1. Arithmetic Operator: +,-,*,/,%,++,--</p>
+<p>2. Assignment Operator: =,+=,-=,*=,/=,%=</p>
+<p>3. Comparison Operator: ==,!=,>,<,>=,<=</p>
+<p>4. Logical Operator: &&,||,!</p>
+<p>5. Bitwise Operator<: &,|,^,~,<<,>></p>
+<p>6. Ternary Operator: ?: </p> </p>
+
+    
+</body>
+</html>
+
+<?php
+
+
+
+
+?>
