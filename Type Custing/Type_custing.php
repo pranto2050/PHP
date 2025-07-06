bd5611bd06c8b402b907652a6e84a0e6f1430907
pranto-islam <prantoislamnt51@gmail.com>
2025-07-07 02:48:12 +0600

    Add PHP files for type casting examples and HTML form structure

--- Type Custing/Type_custing.php
+++ Type Custing/Type_custing.php
@@ -9,10 +9,13 @@ echo var_dump($my_data). "<br>";
 
 
 // Type Custing to Integer Data
-$convart = (int) $my_data;
-// Data Chack
-echo var_dump($convart);
+$convart = (int) $my_data; // Make this data Integer Value
+// Data Chack 
+echo var_dump($convart). "<br>";
 
+$flo = (float) $my_data;
+
+echo var_dump($flo);
 
 
 
